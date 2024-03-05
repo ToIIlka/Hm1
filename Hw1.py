2b1e111bfadca9f2da7d1cbcc7184c6b1340774c
Anton Kredisov <Kredisov031197@gmail.com>
2024-03-05 03:37:38 +0100

    date_change2

--- Hw1.py
+++ Hw1.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 def get_days_from_today(date):
     try:
-        input_data = datetime.strptime(date, "%Y-%m-%d")
+        input_date = datetime.strptime(date, "%Y-%m-%d")
     except ValueError:
         return None
 
     date_today = datetime.today()
-    result = date_today - input_data
+    result = date_today - input_date
     return result.days
 
 print(get_days_from_today("2010-10-09"))
